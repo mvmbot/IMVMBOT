2c619139db13430b58ff28389e7dcac04534a1be
botmvm <148264346+mvmbot@users.noreply.github.com>
2023-11-15 11:14:33 +0100

    IMVM-BOT

--- IMVM-BOT/bot.js
+++ IMVM-BOT/bot.js
@@ -1,34 +1,39 @@
-const { Client, IntentsBitField, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 client.commands = new Collection();
 
-client.commands = new Collection();
+// Carga los comandos
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-  console.log(`✅ Bot is online.`);
-});
+client.once('ready', async () => {
+  console.log(`✅ ${client.user.tag} is online.`);
+  client.user.setPresence({
+    activities: [{ name: `IMVMBOT`, type: 'WATCHING' }],
+    status: 'online',
+  });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
+  // Registra los comandos slash
+  const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+  const commands = client.commands.map(({ data }) => data.toJSON());
 
   try {
-    await command.execute(interaction);
+    console.log('Started refreshing application (/) commands.');
+    await rest.put(
+      Routes.applicationGuildCommands(client.user.id, '1163886056832237718'),
+      { body: commands },
+    );
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
 
---
+++ IMVM-BOT/commands/ban.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ban")
+        .setDescription("Banea a un usuario.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addUserOption(option =>
+            option.setName("user")
+                .setDescription("Usuario a ser baneado.")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("reason")
+                .setDescription("Razon del ban.")
+        ),
+
+    async execute(interaction) {
+        const { channel, options } = interaction;
+
+        const user = options.getUser("user");
+        const reason = options.getString("razon") || "sin razon";
+
+        const member = await interaction.guild.members.fetch(user.id);
+
+        const errEmbed = new EmbedBuilder()
+            .setDescription(`No puedes banear a ${user.username} el tiene un rol superior.`)
+            .setColor(0xc72c3b);
+
+        if (member.roles.highest.position >= interaction.member.roles.highest.position)
+            return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+
+        await member.ban({ reason });
+
+        const embed = new EmbedBuilder()
+            .setTitle(":hammer: Usuario baneado")
+            .setDescription(`${user} ha sido baneado por: ${reason}`)
+            .setColor(0x5fb041)
+            .setTimestamp()
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+    }
+}
