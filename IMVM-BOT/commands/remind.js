f3134b5e06bb9dcb3e53d8cb9d882096426e8712
botmvm <148264346+mvmbot@users.noreply.github.com>
2023-12-15 00:16:42 +0100

    IMVM-UPDATE

--- IMVM-BOT/commands/remind.js
+++ IMVM-BOT/commands/remind.js
@@ -1,5 +1,4 @@
 // remind.js
-<<<<<<< HEAD
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const schedule = require('node-schedule');
 
@@ -36,41 +35,4 @@ module.exports = {
             await interaction.reply('Ingresa una fecha valida.');
         }
     },
-=======
-const schedule = require('node-schedule');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('remind')
-    .setDescription('Set a reminder!')
-    .addStringOption(option =>
-      option.setName('reminder')
-        .setDescription('Reminder you need')
-        .setRequired(true))
-    .addStringOption(option =>
-      option.setName('date')
-        .setDescription('The date when you want it to remind you (formato: DD-MM-AAAA HH:MM:SS)')
-        .setRequired(true)),
-  async execute(interaction) {
-    const reminder = interaction.options.getString('reminder');
-    let dateParts = interaction.options.getString('date').split(' ')[0].split('-');
-    let timeParts = interaction.options.getString('date').split(' ')[1].split(':');
-
-    if (dateParts.length === 3 && timeParts.length === 3) {
-      let date = new Date(dateParts[2], dateParts[1] - 1, dateParts[0], timeParts[0], timeParts[1], timeParts[2]);
-
-      if (!isNaN(date)) {
-        schedule.scheduleJob(date, function(){
-          interaction.user.send(`Reminder: ${reminder}`);
-        });
-        await interaction.reply('Reminder set!');
-      } else {
-        await interaction.reply('Please, choose a valid date!');
-      }
-    } else {
-      await interaction.reply('Please, choose a valid date!');
-    }
-  },
->>>>>>> 0247610e55543d82356cb24ca1a98fe305fffea9
 };
--- IMVM-BOT/commands/study.js
+++ IMVM-BOT/commands/study.js
@@ -1,5 +1,4 @@
 // study.js
-<<<<<<< HEAD
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const schedule = require('node-schedule');
 
@@ -33,41 +32,4 @@ module.exports = {
             await interaction.reply('Ingresa un tiempo de estudio valido.');
         }
     },
-=======
-const schedule = require('node-schedule');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-let studyStep = {}; // Guarda el paso del estudio para cada usuario
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('study')
-    .setDescription('Set a study timer!')
-    .addStringOption(option =>
-      option.setName('time')
-        .setDescription('How long do you want to study? (formato: HH:MM:SS)')
-        .setRequired(true)),
-  async execute(interaction) {
-    const time = interaction.options.getString('time');
-    let timeParts = time.split(':');
-
-    if (timeParts.length === 3) {
-      let date = new Date();
-      date.setHours(date.getHours() + parseInt(timeParts[0]));
-      date.setMinutes(date.getMinutes() + parseInt(timeParts[1]));
-      date.setSeconds(date.getSeconds() + parseInt(timeParts[2]));
-
-      if (!isNaN(date)) {
-        schedule.scheduleJob(date, function(){
-          interaction.user.send(`You already studied a lot, take a break!`);
-        });
-        await interaction.reply('Timer set!');
-      } else {
-        await interaction.reply('Please, choose a valid time!');
-      }
-    } else {
-      await interaction.reply('Please, choose a valid time!');
-    }
-  },
->>>>>>> 0247610e55543d82356cb24ca1a98fe305fffea9
 };
