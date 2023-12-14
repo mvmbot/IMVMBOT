69fe0ef746ace004819d480ee98e0656a176594b
imvmbot <iesmvmbot@gmail.com>
2023-12-14 19:36:10 +0100

    Update signup, doesn't work

--- IMVM-WEB/php/singup.php
+++ IMVM-WEB/php/singup.php
@@ -1,22 +1,18 @@
 <?php
-// Checks if the form uses the 'POST' method.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Gets the data from every input form the form
-    $username = $_POST['username'];
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
-    $confirmPassword = $_POST['cPassword'];
-    //$agreePrivacyPolicy = isset($_POST['privacy_policy']) ? "Yes" : "No";
-
-    // We're printing those to check if it works
-    echo "Username: " . $username . "<br>";
-    echo "Name: " . $name . "<br>";
-    echo "Surname: " . $surname . "<br>";
-    echo "Mail: " . $mail . "<br>";
-    echo "Password: " . $password . "<br>";
-    echo "Confirmed Password: " . $confirmPassword . "<br>";
-    //echo "Agreed to Privacy Policy: " . $agreePrivacyPolicy . "<br>";
-}
+// Gets the data from every input form the form
+$username = $_POST['username'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$confirmPassword = $_POST['cPassword'];
+//$agreePrivacyPolicy = isset($_POST['privacy_policy']) ? 'Yes' : 'No';
+// We're printing those to check if it works
+echo 'Username: ' . $username . '<br>';
+echo 'Name: ' . $name . '<br>';
+echo 'Surname: ' . $surname . '<br>';
+echo 'Mail: ' . $mail . '<br>';
+echo 'Password: ' . $password . '<br>';
+echo 'Confirmed Password: ' . $confirmPassword . '<br>';
+//echo 'Agreed to Privacy Policy: ' . $agreePrivacyPolicy . '<br>';
 ?>
