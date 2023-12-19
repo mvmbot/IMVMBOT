d1cc51298aa36241e75e4bf5bfdec0bc8db90dda
imvmbot <iesmvmbot@gmail.com>
2023-12-19 20:26:06 +0100

    It works for now

--- IMVM-WEB/php/signin-confirm.php
+++ IMVM-WEB/php/signin-confirm.php
@@ -18,6 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
+
     // If something's empty, it ends.
     if (empty($username) || empty($password)) {
         die("Something's wrong! check it out again pls");
@@ -26,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //First of all, we gotta check our database to see if the user exists
     //We do a lil query to check it
     $checkExisting = "SELECT id_users FROM users WHERE username_users = ? AND password_users = ?";
+
     //We prepare the statement
     $stmtCheck = $conn->prepare($checkExisting);
     //Then we bind the parameters (thx manu for the tip, it's actually quite easy to do it this way)
@@ -33,18 +35,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //And then we execute the query
     $stmtCheck->execute();
     //Finaly, we store the result so we can use it
-    $stmtCheck->store_result();
 
-    if ($result->num_rows > 0) {
-        // If its ok, it creates a session!
+    $result = $stmtCheck->get_result();
+    if ($result->fetch_assoc() != null) {
+        // If it's ok, it creates a session!
         $_SESSION['user'] = $username;
         header("Location: ../index.html");
+        exit();
     } else {
         // If the user doesnt exists, we'll redirect it to an error page
         die("Something's wrong! check it out again pls");
     }
-
-    $conn->close();
 } else {
     // If someone goes into the page without sending the form, we redirect them
     header("Location: ./signin.html");
