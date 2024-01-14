7d7e7641e0f1d7fd7bdb036e0b50f618f7318f95
botmvm <148264346+mvmbot@users.noreply.github.com>
2024-01-14 17:11:53 +0100

    IMVMBOT
    emojify command

---
+++ IMVM-BOT/commands/emojify.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('emojify')
+    .setDescription('Convierte texto en emojis')
+    .addStringOption(option =>
+      option.setName('mensaje')
+        .setDescription('Mensaje para convertir en emojis')
+        .setRequired(true)), // Cambia a false si quieres que sea opcional
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    const args = interaction.options.getString('mensaje');
+
+    if (!args) {
+      interaction.reply('Debes proporcionar un mensaje para transformar en emojis.');
+      return;
+    }
+
+    const emojiText = emojify(args);
+    interaction.followUp(emojiText);
+  },
+};
+
+function emojify(text) {
+  const characters = [...text];
+  const emojiCharacters = characters.map((char) => {
+    if (/^\s+$/.test(char)) {
+      // Mantener los espacios en blanco sin cambios
+      return '   ';
+    } else {
+      // Convertir cada letra en un emoji cuadrado de esa letra
+      return `:regional_indicator_${char.toLowerCase()}:`;
+    }
+  });
+
+  return emojiCharacters.join(' ');
+}
