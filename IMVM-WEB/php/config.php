d8a9bb01ce82f30538e3d4daae5b37d15a43d6ca
JisuKlk <148264346+mvmbot@users.noreply.github.com>
2024-01-29 01:57:38 +0100

    Update config.php

--- IMVM-WEB/php/config.php
+++ IMVM-WEB/php/config.php
@@ -11,5 +11,3 @@ define('DB_NAME', 'id21626905_imvmbotdb');
 $clientID = '818127934520-j7stjk45cqe9ksn8kbskgsgquurlitn7.apps.googleusercontent.com';
 $clientSecret = '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';
 $redirectUri = 'https://imvmbot.000webhostapp.com/index.html';
-
-?>
