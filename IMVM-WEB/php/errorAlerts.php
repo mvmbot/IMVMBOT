2313fa590cc5f56e15c9105f9c7e41b060991a79
JisuKlk <148264346+mvmbot@users.noreply.github.com>
2024-01-29 01:57:50 +0100

    Update errorAlerts.php
    New alerts

--- IMVM-WEB/php/errorAlerts.php
+++ IMVM-WEB/php/errorAlerts.php
@@ -6,3 +6,17 @@ function showAlert($errorMessage) {
 function showError($errorMessage) {
     echo "Error: " . $errorMessage;
 }
+
+function showErrorPasswordJS() {
+    echo'<script type="text/javascript">
+    alert("Error: incorrect Password");
+    window.location.href="../signin.php";
+    </script>';
+}
+
+function showErrorUser() {
+    echo`<script type="text/javascript">
+    alert("Error: User doesn't exists");
+    window.location.href="../signin.php";
+    </script>`;
+}
