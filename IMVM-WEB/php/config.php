9132eaf110cf3858ab0321f719fda04ab87f4dce
afernandezmvm <114910821+afernandezmvm@users.noreply.github.com>
2024-02-20 17:58:22 +0000

    New data for our database

--- IMVM-WEB/php/config.php
+++ IMVM-WEB/php/config.php
@@ -1,9 +1,9 @@
 <?php
 //Database info
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'id21626905_imvmbotuser');
+define('DB_USERNAME', 'id21626905_adminmvm');
 define('DB_PASSWORD', 'BaconFrito33.');
-define('DB_NAME', 'id21626905_imvmbotdb');
+define('DB_NAME', 'id21626905_imvmdatabase');
 
 // CONFIGURACION DE GOOGLE
 $clientID = '818127934520-j7stjk45cqe9ksn8kbskgsgquurlitn7.apps.googleusercontent.com';
