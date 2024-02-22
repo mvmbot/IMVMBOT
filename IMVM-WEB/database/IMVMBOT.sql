53c80935bb3f5bf530de899fc9a6d58a67804a5b
afernandezmvm <114910821+afernandezmvm@users.noreply.github.com>
2024-02-22 18:39:53 +0000

    Base code

--- IMVM-WEB/database/IMVMBOT.sql
+++ IMVM-WEB/database/IMVMBOT.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IMVMBOT;
 -- Tabla para almacenar administradores
 CREATE TABLE admin (
     idAdmin INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    usernameAdminC VARCHAR(50) NOT NULL UNIQUE,
+    usernameAdmin VARCHAR(50) NOT NULL UNIQUE,
     nameAdmin VARCHAR(50) NOT NULL,
     surnameAdmin VARCHAR(50) NOT NULL UNIQUE,
     emailAdmin VARCHAR(100) NOT NULL UNIQUE,
@@ -24,97 +24,89 @@ CREATE TABLE users (
 -- Tabla para almacenar la información de los tickets.
 CREATE TABLE ticket (
     idTicket INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    idUsers INT,
     typeTicket ENUM('Help & Support', 'Bug Reporting', 'Feature Request', 'Abuse Report', 'General Inquiry', 'Improvement Suggestions', 'Grammar/Translation Issues', 'Information Update', 'Other') NOT NULL,
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modificationDate DATE NULL,
     resolvedDate TIMESTAMP,
     stateTicket ENUM('Open', 'In progress', 'Closed') DEFAULT 'In progress',
-    attachmentTicket VARCHAR(225),
-    affairTicket VARCHAR(100) NOT NULL,
-    descriptionTicket TEXT NOT NULL,
-    bugTicket1 TEXT NOT NULL,
-    bugTicket2 TEXT NOT NULL,
-    bugTicket3 TEXT NOT NULL,
+    idUsers INT,
     FOREIGN KEY (idUsers) REFERENCES users(idUsers)
 );
 
 -- Help & Support
 CREATE TABLE `Help & Support` (
-    ticketID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     typeTicket VARCHAR(50),
     subject VARCHAR(255),
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Bug Reporting
 CREATE TABLE `Bug Reporting` (
-    bugReportID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
     description TEXT,
-    image VARCHAR(100)
+    image VARCHAR(255),
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
-
 
 -- Feature Request
 CREATE TABLE `Feature Request` (
-    featureRequestID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Abuse Report
 CREATE TABLE `Abuse Report` (
-    abuseReportID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT,
-    image VARCHAR(100)
+    description TEXT.
+    image VARCHAR(255),
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- General Inquiry
 CREATE TABLE `General Inquiry` (
-    generalInquiryID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Improvement Suggestions
 CREATE TABLE `Improvement Suggestions` (
-    improvementID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Grammar/Translation Issues
 CREATE TABLE `Grammar/Translation Issues` (
-    grammarTranslationID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
     description TEXT,
-    image VARCHAR(100)
+    image VARCHAR(255),
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Information Update
 CREATE TABLE `Information Update` (
-    informationUpdateID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Other
 CREATE TABLE `Other` (
-    otherID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
+    ticketID INT NOT NULL,
     subject VARCHAR(255),
-    description TEXT
-    
+    description TEXT,
+    FOREIGN KEY (ticketID) REFERENCES ticket(idTicket)
 );
 
 -- Tabla para las respuestas de los admins y usuarios
@@ -134,7 +126,6 @@ CREATE TABLE response (
 -- Jobs Page
 CREATE TABLE `Collaboration/Development` (
     collaborationID INT PRIMARY KEY AUTO_INCREMENT,
-    userID INT,
     name VARCHAR(30),
     surname VARCHAR(30),
     email VARCHAR(50),
