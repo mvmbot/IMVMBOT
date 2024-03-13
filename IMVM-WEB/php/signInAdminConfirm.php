2f3161273b969e1b2491d10830fcb3478cca10ca
afernandezmvm <afernandez2@institutmvm.cat>
2024-03-13 10:02:15 +0100

    Better comment format

--- IMVM-WEB/php/signInAdminConfirm.php
+++ IMVM-WEB/php/signInAdminConfirm.php
@@ -1,19 +1,19 @@
 <?php
-// Let's make sure we're keeping track of the user's info
+# Let's make sure we're keeping track of the user's info
 session_start();
 
-#region Required files
-// Now, time to set up the connection to our awesome database
+#region Required files --- Everything we need on this code
+# Now, we get all the database stuff
 require("config.php");
 require("databaseFunctions.php");
 
-// Grab some handy tools for our code
+# Grab some handy tools for our code
 require("redirectFunctions.php");
 require("dataValidationFunctions.php");
 require("errorAlerts.php");
 #endregion
 
-#region errors --- Turn on the lights to catch any potential coding errors
+#region errors --- Turn on the error reporting to catch any potential coding errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 #endregion
@@ -21,60 +21,58 @@ ini_set('display_errors', 1);
 #region 'IF-ELSE' --- First things first, let's check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Checking our connection to the database!
+    # Checking our connection to the database!
     $conn = connectToDatabase();
 
     #region Variable Declaration
-    // Getting the username and password entered in the form
+    # Getting the username and password entered in the form
     $username = $_POST['usernameAdmin'] ?? '';
     $password = $_POST['passwordAdmin'] ?? '';
 
-    // Let's make sure they filled out all the important fields
+    # We store the data from the user on a new var to check if they're empty or not
     $fieldsToCheck = ['usernameAdmin', 'passwordAdmin'];
     #endregion
 
-    // If any of these is empty, let's kindly ask them to try again
+    # If any of these is empty, we redirect them to the sign in so they can try again
     if (areFieldsEmpty($fieldsToCheck)) {
         redirectToSignin();
     }
 
-    // Step 1: Let's check if the user exists in our awesome database
-
-    // We start with a quick query to find out
+    # We start with a quick query to find out if the user exists on the database
     $checkExisting = "SELECT idAdmin, passwordAdmin FROM admin WHERE usernameAdmin = ?";
 
-    // Preparing the statement for the big moment
+    # Preparing the statement
     $stmtCheck = $conn->prepare($checkExisting);
 
-    // Now, we're getting ready to bind the parameters
+    # Now, we're getting ready to bind the parameters
     $stmtCheck->bind_param("s", $username);
 
-    // Alright, time to execute the query and see what the database tells us
+    # Alright, time to execute the query and see what the database tells us
     if ($stmtCheck->execute()) {
-        // Got the result, storing it in a variable for some magic
+        # Got the result, storing it in a variable
         $result = $stmtCheck->get_result();
 
-        // Did we find anything in the query?
+        # Did we find anything in the query?
         if ($result->num_rows > 0) {
 
-            // Bingo! Passwords match, time to create a VIP session for this user
+            # Passwords match, create a session for this user
             $_SESSION['admin'] = $username;
             redirectToIndex();
         } else {
-            // No luck in the query, informing the user and inviting them to try again
+            # No luck in the query, informing the user and redirecting them to the form again
             showErrorUserJS();
         }
     } else {
 
-        // Something went wrong with the query execution, showing a helpful error message
+        # Query execution didnt work, display an error message
         showError("Something went wrong: " . $stmtCheck->error);
     }
 
-    // Finally, let's close the database connection
+    # We're done, close the connection
     closeDatabaseConnection($conn);
 } else {
 
-    // No POST method detected, gently guiding the user to the signin page
+    # No POST method detected, guiding the user to the signin page (we dont let them get here without the POST)
     redirectToSignin();
 }
 #endregion
