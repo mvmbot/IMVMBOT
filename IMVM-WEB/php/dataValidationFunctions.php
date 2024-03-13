d8de468a6cfc1afba4c285aab297d15ca0615467
afernandezmvm <afernandez2@institutmvm.cat>
2024-03-13 08:41:50 +0100

    Better comment

--- IMVM-WEB/php/dataValidationFunctions.php
+++ IMVM-WEB/php/dataValidationFunctions.php
@@ -2,13 +2,17 @@
 
 #region function --- Simple function to check if there are empty values
 function areFieldsEmpty($fields) {
-    // We gotta check if any value is empty
+
+    # We gotta check if any value is empty
     foreach ($fields as $field) {
+
+        # In case there's something empty, we return 'true'
         if (empty($_POST[$field])) {
             return true;
         }
     }
-    // If not, everything's allright, go on
+
+    # If not, everything's allright, return 'false'
     return false;
 }
 #endregion
