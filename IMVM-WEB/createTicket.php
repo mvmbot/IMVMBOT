fd95b92de5c7304aaa0b7f5e5d536c8544a42e61
afernandezmvm <afernandez2@institutmvm.cat>
2024-03-13 10:54:26 +0100

    Now the submit button is actually a submit

--- IMVM-WEB/createTicket.php
+++ IMVM-WEB/createTicket.php
@@ -416,7 +416,7 @@ session_start();
 
         <!-- Div for the submit button -->
         <div style="margin-top: 10px;">
-            <button type="button" class="btn btn-primary py-2 px-3" action="submitForm()">SUBMIT</button>
+            <button type="submit" class="btn btn-primary py-2 px-3" action="submitForm()">SUBMIT</button>
         </div>
     </form>
 
