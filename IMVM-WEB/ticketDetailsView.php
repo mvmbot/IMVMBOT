4b6d1aa2312c9c1a30005fd0fba053d55d38c14f
afernandezmvm <74646859+JisuKlk@users.noreply.github.com>
2024-03-21 22:23:18 +0100

    Type is now received via URL properly

--- IMVM-WEB/ticketDetailsView.php
+++ IMVM-WEB/ticketDetailsView.php
@@ -289,7 +289,7 @@ error_reporting(E_ALL);
     <!-- Navbar End -->
     <?php
     $conn = connectToDatabase();
-    $type = intval($_GET['type']);
+    $type = $_GET['type'];
     ?>
     <br><br>
     <center><h1>Ticket Detail</h1>
