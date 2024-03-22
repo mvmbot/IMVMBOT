c205b2a401223f55634a8ec16fd8c8fbed2b3649
afernandezmvm <74646859+JisuKlk@users.noreply.github.com>
2024-03-22 02:26:55 +0100

    Whole new function to edit the tickets

---
+++ IMVM-WEB/php/editTicketData.php
@@ -0,0 +1,34 @@
+<?php
+require("redirectFunctions.php");
+require("databaseFunctions.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $conn = connectToDatabase();
+    $newState = $_POST['newState'];
+    $idTicket = $_POST['idTicket'];
+    try {
+        $sql = "UPDATE ticket SET stateTicket = ?, modificationDate = CURRENT_TIMESTAMP WHERE idTicket = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("si", $newState, $idTicket);
+        $stmt->execute();
+        if ($newState === "Closed") {
+            try {
+                $sql = "UPDATE ticket SET resolvedDate = CURRENT_TIMESTAMP WHERE idTicket = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("i", $idTicket);
+                $stmt->execute();
+            } catch (Exception $e) {
+                showError("Error: " . $e->getMessage());
+                redirectToViewTicketAdmin();
+            }
+        }
+        if ($stmt->execute()) {
+            redirectToViewTicketAdmin();
+        }
+    } catch (Exception $e) {
+        $stmt->close();
+        showError("Error: " . $e->getMessage());
+        redirectToSignup();
+    }
+} else {
+    redirectToViewTicketAdmin();
+}
