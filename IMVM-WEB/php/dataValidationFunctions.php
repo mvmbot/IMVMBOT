5a2fec5979bb5a791237f8ba590caaf79c07261e
afernandezmvm <74646859+JisuKlk@users.noreply.github.com>
2024-03-26 02:11:02 +0100

    Modification on sanitize method to accept single values instead of just arrays, still to check

--- IMVM-WEB/php/dataValidationFunctions.php
+++ IMVM-WEB/php/dataValidationFunctions.php
@@ -1,14 +1,24 @@
 <?php
 
-#region function --- Simple function to check if there are empty values and sanitize them
+#region function --- Simple function to check if there are empty values and sanitize them;
 function sanitizeInputsAndCheckEmpty($inputs) {
-    $sanitizedInputs = array();
-    foreach ($inputs as $inputValue) {
-        $sanitizedInputs[$inputValue] = isset($_POST[$inputValue]) ? htmlspecialchars($_POST[$inputValue]) : '';
-        if (empty($sanitizedInputs[$inputValue])) {
-            return true; // Return true if any field is empty
+
+    if (!is_array($inputs)) {
+        # If the input is not an array (it's just a single var), we sanitize it this way (so we don't neet to create arrays with one value);
+        return isset($_POST[$inputs]) ? htmlspecialchars($_POST[$inputs]) : "";
+    } else {
+        # If it's an array, we make a new array that will grab every sanitized input
+        $sanitizedInputs = array();
+        foreach ($inputs as $inputValue) {
+            # This is how we sanitize it, first we check if they're empty, if it's not empty, we use the htmlspecialchars method to sanitize it
+            $sanitizedInputs[$inputValue] = isset($_POST[$inputValue]) ? htmlspecialchars($_POST[$inputValue]) : '';
+            # Otherwise, we return true, as it's empty
+            if (empty($sanitizedInputs[$inputValue])) {
+                return true;
+            }
         }
+        # Then we just return the new array with every sanitized input
+        return $sanitizedInputs;
     }
-    return $sanitizedInputs; // Return sanitized inputs if all fields are filled
 }
 #endregion
