74e213c90202debc1296ae45a684834ece520f72
afernandezmvm <74646859+JisuKlk@users.noreply.github.com>
2024-03-27 05:07:31 +0100

    Simple file management for tickets i made to work on

--- IMVM-WEB/php/createTicketConfirm.php
+++ IMVM-WEB/php/createTicketConfirm.php
@@ -18,15 +18,21 @@ ini_set('display_errors', 1);
 # Now, let's connect to our database
 $conn = connectToDatabase();
 
-# We grab all the data from the form
-$type = $_POST['type'] ?? ''; # First of all, we get the type so we know what table to look in
-$fieldsToCheck; # This will hold an array with fields that need validation
+# We declare the directory that we're using to store all the user data
+$targetDirectory =  './userUploads/';
 
-$targetDirectory =  './userUploads/'; # Directory where images should
+# We grab all the data from the form
+
+# First of all, we get the type so we know what table to look in
+$type = $_POST['type'] ?? '';
+
+# This will hold an array with fields that need validation
+$fieldsToCheck;
+
 # Switch case to fill the values from every possible type
 switch ($type) {
 
-        #region Help & Support
+    #region Help & Support
     case 'helpSupport':
         $subject = $_POST['subjectHelpSupportFields'] ?? '';
         $description = $_POST['descriptionHelpSupportFields'] ?? '';
@@ -36,9 +42,58 @@ switch ($type) {
             $description
         );
 
+        # Check if the user has filled out everything necessary (just the necessary, there can be null values sometimes), and for the newer version, sanitize them aswell!\
         $varCheck = sanitizeInputsAndCheckEmpty($inputs);
 
-        #$fileAttachment = $targetDirectory . basename($_FILES["fileAttachmentHelpSupportFields"]);
+        # Declare the file attachment path
+        $fileAttachment = $targetDirectory . basename($_FILES["fileAttachmentHelpSupportFields"]["name"]);
+
+        # We'll be using this var to check if the file uploadede correctly
+        $uploadOk = 1;
+
+        # Now we get the extension of the file (in lower case)
+        $imageFileType = strtolower(pathinfo($fileAttachment, PATHINFO_EXTENSION));
+
+        # Then, we gotta check if it's a real image
+        if(isset($_POST["fileAttachmentHelpSupportFields"])) {
+            $check = getimagesize($_FILES["fileAttachmentHelpSupportFields"]["tmpName"]);
+            if(!$check) {
+                echo 'file is an image - ' . $check['mime'] . '.';
+                $uploadOk = 1;
+            } else {
+                echo "Sorry, only images are allowed.";
+                $uploadOk = 0;
+            }
+        }
+
+        # We also gotta check if the image already exists
+        if (file_exists($fileAttachment)) {
+            echo "Sorry, your file already exists.";
+            $uploadOk = 0;
+        }
+
+        # Now we check the file size
+        if ($_FILES["fileAttachmentHelpSupportFields"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        # Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"){
+            echo "Sorry, only JPG/JPEG and PNG files are allowed.";
+            $uploadOk = 0;
+        }
+
+        # If uploadOk = 0, there's an error
+        if ($uploadOk == 0) {
+            echo 'file not uploaded';
+        } else {
+            if (move_uploaded_file($_FILES["fileAttachmentHelpSupportFields"]["tmp_name"], $fileAttachment)) {
+                echo "The file " . htmlspecialchars(basename ($_FILES["fileAttachmentHelpSupportFields"]["name"])) . "has been uploaded";
+            } else {
+                echo "There was an error uploading your file";
+            }
+        }
 
         # Check if the user has filled out everything necessary (just the necessary, there can be null values sometimes)
         if ($varCheck === true) {
@@ -47,10 +102,10 @@ switch ($type) {
         # Now we create the Ticket with the parameters we just took from the user's form
         createTicketHelpSupport($conn, $subject, $fileAttachment, $description);
 
-        break;
-        #endregion
+    break;
+    #endregion
 
-        #region Bug Reporting
+    #region Bug Reporting
     case 'bugReport':
         $requestType = $_POST['requestTypeBugReportFields'] ?? '';
         $subject = $_POST['subjectBugReportFields'] ?? '';
@@ -72,7 +127,6 @@ switch ($type) {
             $bugImage
         );
 
-        # Check if the user has filled out everything necessary (just the necessary, there can be null values sometimes), and for the newer version, sanitize them aswell!
         $varCheck = sanitizeInputsAndCheckEmpty($inputs);
 
         if ($varCheck === true) {
@@ -81,10 +135,10 @@ switch ($type) {
         # Now we create the Ticket with the parameters we just took from the user's form
         createTicketBugReport($conn, $requestType, $subject, $bugDescription, $stepsToReproduce, $expectedResult, $receivedResult, $discordClient, $bugImage);
 
-        break;
-        #endregion
+    break;
+    #endregion
 
-        #region  Feature Request
+    #region  Feature Request
     case 'featureRequest':
         $requestType = $_POST['requestTypeFeatureRequestFields'] ?? '';
         $subject = $_POST['subjectFeatureRequestFields'] ?? '';
@@ -104,10 +158,10 @@ switch ($type) {
             # Now we create the Ticket with the parameters we just took from the user's form
             createTicketFeatureRequest($conn, $requestType, $subject, $description);
         }
-        break;
-        #endregion
+    break;
+    #endregion
 
-        #region Grammar Issues
+    #region Grammar Issues
     case 'grammarIssues':
         $subject = $_POST['subjectGrammarIssuesFields'] ?? '';
         $description = $_POST['descriptionGrammarIssuesFields'] ?? '';
@@ -126,10 +180,10 @@ switch ($type) {
         # Now we create the Ticket with the parameters we just took from the user's form
         createTicketGrammarIssues($conn, $subject, $description, $fileAttachment);
 
-        break;
-        #endregion
+    break;
+    #endregion
 
-        #region Information Update
+    #region Information Update
     case 'informationUpdate':
         $subject = $_POST['subjectInformationUpdateFields'] ?? '';
         $updateInfo = $_POST['updateInfoInformationUpdateFields'] ?? '';
@@ -147,10 +201,10 @@ switch ($type) {
         # Now we create the Ticket with the parameters we just took from the user's form
         createTicketInformationUpdate($conn, $subject, $updateInfo);
 
-        break;
-        #endregion
+    break;
+    #endregion
 
-        #region Other Issues
+    #region Other Issues
     case 'other':
         $subject = $_POST['subjectOtherFields'] ?? '';
         $description = $_POST['descriptionOtherFields'] ?? '';
@@ -171,11 +225,11 @@ switch ($type) {
         createTicketOther($conn, $subject, $description, $extraText);
 
         break;
-        #endregion
+    #endregion
 
         #region Default
     default:
         break;
-        #endregion
+    #endregion
 }
 redirectToViewTicket();
