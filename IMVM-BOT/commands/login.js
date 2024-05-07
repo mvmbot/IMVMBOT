39a7da335f8f52f26222d2556fdf5e6bc30bfa54
ivanmvm <isaez@institutmvm.cat>
2024-05-07 20:09:35 +0200

    login.js working bbdd

--- IMVM-BOT/commands/login.js
+++ IMVM-BOT/commands/login.js
@@ -1,72 +1,75 @@
-// login.js
+  // login.js
 
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { google } = require('googleapis');
-const mysql = require('mysql');
-require('dotenv').config();
+  const { SlashCommandBuilder } = require('@discordjs/builders');
+  const { google } = require('googleapis');
+  const mysql = require('mysql');
+  require('dotenv').config();
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
-);
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('login')
-    .setDescription('Log in to Google Classroom'),
-  async execute(interaction) {
-    const authorizationUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: [
-        'https://www.googleapis.com/auth/classroom.announcements',
-        'https://www.googleapis.com/auth/classroom.courses',
-        'https://www.googleapis.com/auth/classroom.coursework.me',
-        'https://www.googleapis.com/auth/classroom.guardianlinks.me.readonly'
-      ],
-    });
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('login')
+      .setDescription('Log in to Google Classroom'),
+    async execute(interaction) {
+      const state = JSON.stringify({ discordUserId: interaction.user.id });
 
-    await interaction.reply({
-      ephemeral: true,
-      embeds: [
-        {
-          title: 'Google Classroom Login',
-          description: 'Please log in to Google Classroom by clicking the button below. You will be redirected to a web page to complete the login process.',
-          fields: [
-            {
-              name: 'Authorization URL',
-              value: `[Click here to Sign In](${authorizationUrl})`
-            }
-          ],
-          color: 0x008000,
-          footer: {
-            text: 'You will be redirected to Google to authorize the IMVMBOT application.'
+      const authorizationUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: [
+          'https://www.googleapis.com/auth/classroom.announcements',
+          'https://www.googleapis.com/auth/classroom.courses',
+          'https://www.googleapis.com/auth/classroom.coursework.me',
+          'https://www.googleapis.com/auth/classroom.guardianlinks.me.readonly'
+        ],
+        state: state
+      });
+
+      await interaction.reply({
+        ephemeral: true,
+        embeds: [
+          {
+            title: 'Google Classroom Login',
+            description: 'Please log in to Google Classroom by clicking the button below. You will be redirected to a web page to complete the login process.',
+            fields: [
+              {
+                name: 'Authorization URL',
+                value: `[Click here to Sign In](${authorizationUrl})`
+              }
+            ],
+            color: 0x008000,
+            footer: {
+              text: 'You will be redirected to Google to authorize the IMVMBOT application.'
+            },
+            image: {
+              url: 'https://cdn.discordapp.com/attachments/1054482794392338502/1219741630895100054/imvmbot-classroom.jpg',
+            },
           },
-          image: {
-            url: 'https://cdn.discordapp.com/attachments/1054482794392338502/1219741630895100054/imvmbot-classroom.jpg',
-          },
-        },
-      ],
-    });
+        ],
+      });
 
-    const filter = (m) => m.author.id === interaction.user.id;
-    const collector = interaction.channel.createMessageCollector({ filter, time: 60000 });
+      const filter = (m) => m.author.id === interaction.user.id;
+      const collector = interaction.channel.createMessageCollector({ filter, time: 60000 });
 
-    collector.on('collect', async (m) => {
-      const code = m.content;
-      const { tokens } = await oauth2Client.getToken(code);
-      const accessToken = tokens.access_token;
+      collector.on('collect', async (m) => {
+        const code = m.content;
+        const { tokens } = await oauth2Client.getToken(code);
+        const accessToken = tokens.access_token;
 
-      saveAccessTokenToDatabase(interaction.user.id, accessToken);
+        saveAccessTokenToDatabase(interaction.user.id, accessToken);
 
-      await interaction.followUp('Token de acceso guardado en la base de datos.');
-      collector.stop();
-    });
+        await interaction.followUp('Token de acceso guardado en la base de datos.');
+        collector.stop();
+      });
 
-    collector.on('end', (collected, reason) => {
-      if (reason === 'time') {
-        interaction.followUp('Tiempo de espera agotado. Por favor, intenta de nuevo.');
-      }
-    });
-  },
-};
+      collector.on('end', (collected, reason) => {
+        if (reason === 'time') {
+          interaction.followUp('Tiempo de espera agotado. Por favor, intenta de nuevo.');
+        }
+      });
+    },
+  };
