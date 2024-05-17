8771c9988f23aff7f0fbe91429abf7729722d6f4
afernandezmvm <114910821+afernandezmvm@users.noreply.github.com>
2024-05-17 17:24:46 +0000

    Everything commented properly

--- IMVM-BOT/commands/github.js
+++ IMVM-BOT/commands/github.js
@@ -1,6 +1,8 @@
+//#region Requires
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios');
-const { EmbedBuilder } = require('discord.js'); // Corrected import for the embed builder
+const { EmbedBuilder } = require('discord.js');
+//#endregion
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,9 +12,11 @@ module.exports = {
       option.setName('search_term')
         .setDescription('Término de búsqueda')
         .setRequired(true)),
+  // We ask the user for a term to search as a repository
   async execute(interaction) {
     const searchTerm = interaction.options.getString('search_term');
     if (!searchTerm) {
+      // In case it's empty we tell the user we really need something to search for
       return interaction.reply({ content: 'Por favor proporciona un término de búsqueda.', ephemeral: true });
     }
 
@@ -21,7 +25,7 @@ module.exports = {
       const response = await axios.get(`https://api.github.com/search/repositories?q=${encodeURIComponent(searchTerm)}&per_page=5`, {
         headers: {
           'User-Agent': 'mvmbot',
-          'Accept': 'application/vnd.github.v3+json' // Use the latest version of the GitHub API
+          'Accept': 'application/vnd.github.v3+json'
         }
       });
 
@@ -40,6 +44,7 @@ module.exports = {
         .setDescription('Aquí están los primeros 5 resultados:')
         .setFooter({ text: 'IMVMBOT Search', iconURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/200px-GitHub_Invertocat_Logo.svg.png' });
 
+      // We use a forEach to iterate the results and print some info
       response.data.items.forEach((item, index) => {
         embed.addFields({ 
           name: `Resultado ${index + 1}`, 
@@ -47,10 +52,11 @@ module.exports = {
         });
       });
 
+      // Finally, we send the embed to the user
       await interaction.reply({ embeds: [embed] });
     } catch (error) {
+      // In case somethings wrong, here's lotta logs ^*^
       console.error("Error al buscar en GitHub:", error);
-
       if (error.response) {
         console.error("Detalles del error de respuesta:", error.response.data);
       } else if (error.request) {
@@ -58,7 +64,7 @@ module.exports = {
       } else {
         console.error("Error durante la configuración de la solicitud:", error.message);
       }
-
+      // Since logs are for us, the devs, we gotta print something for the user, so we tell the user there's something wrong
       await interaction.reply({ content: 'Hubo un error al buscar en GitHub. Por favor, inténtalo de nuevo más tarde.', ephemeral: true });
     }
   },
