0d577e2460dc9fd6c505a0fd742d67c2b1a398c4
afernandezmvm <114910821+afernandezmvm@users.noreply.github.com>
2024-05-17 17:04:17 +0000

    Error handling

--- IMVM-BOT/commands/github.js
+++ IMVM-BOT/commands/github.js
@@ -38,12 +38,20 @@ module.exports = {
         .setFooter('IMVMBOT Search', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/200px-GitHub_Invertocat_Logo.svg.png');
 
       response.data.items.forEach((item, index) => {
-        embed.addField(`Resultado ${index + 1}`, `[${item.full_name}](${item.html_url}) - ${item.description || 'No description provided'}`);
+        embed.addField(
+          `Resultado ${index + 1}`,
+          `[${item.full_name}](${item.html_url}) - ${item.description || 'No description provided'}`
+        );
       });
 
       await interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.error("Error al buscar en GitHub:", error.message);
+      if (error.response) {
+        console.error("Detalles del error de respuesta:", error.response.data);
+      } else if (error.request) {
+        console.error("No se recibió respuesta del servidor de GitHub:", error.request);
+      }
       await interaction.reply({ content: 'Hubo un error al buscar en GitHub. Por favor, inténtalo de nuevo más tarde.', ephemeral: true });
     }
   },
